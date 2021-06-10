950e2d65f94ae5bb1e734d52877272eee1da89be
laribrito <lary.29.ds@gmail.com>
2021-06-10 07:49:12 -0300

    calculadora esta funcionando

--- telaCf.py
+++ telaCf.py
@@ -1,6 +1,29 @@
-from PyQt5 import QtWidgets, uic
+from PyQt5 import QtWidgets, uic, QtCore
+from PyQt5.QtCore import QStringListModel
 
 class Cf(QtWidgets.QMainWindow):
 	def __init__(self):
 		super().__init__()
 		uic.loadUi("telas/telaCf.ui", self)
+
+		self.btnVoltar = self.findChild(QtWidgets.QPushButton, "voltar")
+		self.btnVoltar.clicked.connect(self.fecharJanela)
+
+		self.btnCalcular = self.findChild(QtWidgets.QPushButton, "converter")
+		self.btnCalcular.clicked.connect(self.calcular)
+
+		self.esse = self.findChild(QtWidgets.QLineEdit, "esse")
+
+		self.saida = self.findChild(QtWidgets.QLabel, "resultado")
+
+	def fecharJanela(self):
+		self.close()
+		self.esse.setText("")
+		self.saida.setText("")
+
+	def calcular(self):
+		self.c = self.esse.text()
+		self.c = float(self.c)
+		self.faren = ((self.c * 9)/5) + 32
+		self.faren = str(self.faren)
+		self.saida.setText(self.faren)
--- telaFc.py
+++ telaFc.py
@@ -4,3 +4,25 @@ class Fc(QtWidgets.QMainWindow):
 	def __init__(self):
 		super().__init__()
 		uic.loadUi("telas/telaFc.ui", self)
+
+		self.btnVoltar = self.findChild(QtWidgets.QPushButton, "voltar")
+		self.btnVoltar.clicked.connect(self.fecharJanela)
+
+		self.btnCalcular = self.findChild(QtWidgets.QPushButton, "converter")
+		self.btnCalcular.clicked.connect(self.calcular)
+
+		self.esse = self.findChild(QtWidgets.QLineEdit, "esse")
+
+		self.saida = self.findChild(QtWidgets.QLabel, "resultado")
+
+	def fecharJanela(self):
+		self.close()
+		self.esse.setText("")
+		self.saida.setText("")
+
+	def calcular(self):
+		self.f = self.esse.text()
+		self.f = float(self.f)
+		self.cels = 5 * ((self.f-32) / 9)
+		self.cels = str(self.cels)
+		self.saida.setText(self.cels)
