d3f0a2ca6a6b1e0c7b3ba2c9966f55cc029a9013
laribrito <lary.29.ds@gmail.com>
2021-10-01 18:03:37 -0300

    Remove arquivo 09.py

--- 09.py
+++
@@ -1,5 +0,0 @@
-# coding: utf-8
-print("- Conversão de temperatura (F para C) - ")
-f = float(input("Temperatura em Fahrenheint>\n > "))
-c = 5 * ((f-32) / 9)
-print(f"A temperatura em Celsius  é {c}ºC.")
